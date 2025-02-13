7e07c1db23f607f2a578e35bc2f3b4fa1031c25d
Shane Rogers <shane@repllabs.com>
2025-02-12 17:17:56 -0800

    Tweak config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     rollupOptions: isSsrBuild
       ? {
-          input: "./workers/app.ts",
+          input: './workers/app.ts',
         }
       : undefined,
+  },
+  ssr: {
+    optimizeDeps: {
+      include: ['react-dom/server'],
+    },
   },
   plugins: [
     cloudflareDevProxy({
       getLoadContext({ context }) {
-        return { cloudflare: context.cloudflare };
+        return { cloudflare: context.cloudflare }
       },
     }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
   ],
-}));
+}))
