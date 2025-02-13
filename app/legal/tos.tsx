a0cb0d1168c7251e31358277abd5f992eed7a6da
Shane Rogers <shane@repllabs.com>
2025-02-12 17:35:32 -0800

    Add markdown renderer

--- app/legal/tos.tsx
+++ app/legal/tos.tsx
@@ -1,3 +1,40 @@
-export default function TosIndex() {
-  return <div className="text-2xl text-slate-950">TOS</div>
+import type { Route } from './+types/tos'
+import { parse } from '~/services/markdoc.server'
+import { Container } from '~/components/Marketing/container'
+import { Markdown } from '~/components/Markdown'
+
+export async function loader() {
+  const MARKDOWN = `---
+title: What is Markdoc?
+---
+
+# {% $markdoc.frontmatter.title %} {% #overview %}
+
+Markdoc is a Markdown-based syntax and toolchain for creating custom documentation sites. Stripe created Markdoc to power [our public docs](http://stripe.com/docs).
+
+{% callout type="check" %}
+Markdoc is open-source—check out its [source](http://github.com/markdoc/markdoc) to see how it works.
+{% /callout %}
+
+## How is Markdoc different?
+
+Markdoc uses a fully declarative approach to composition and flow control, where other solutions… [Read more](/docs/overview).
+
+## Next steps
+- [Install Markdoc](/docs/getting-started)
+- [Explore the syntax](/docs/syntax)
+`
+  return { content: parse(MARKDOWN) }
+}
+
+export default function TosIndex({
+  loaderData,
+}: {
+  loaderData: Route.ComponentProps
+}) {
+  return (
+    <Container className="mt-24 sm:mt-32 md:mt-56 text-black">
+      <Markdown content={loaderData.content} />
+    </Container>
+  )
 }
